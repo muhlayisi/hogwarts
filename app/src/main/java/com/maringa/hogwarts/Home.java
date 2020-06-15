b720733026a4c860532f427078c96ae57cf7d394
Muhlayisi Maringa <muhlayisi.maringa@gmail.com>
2020-06-15 13:07:48 +0200

    Added new activity for hogwarts houses

--- app/src/main/java/com/maringa/hogwarts/Home.java
+++ app/src/main/java/com/maringa/hogwarts/Home.java
@@ -1,14 +1,39 @@
 package com.maringa.hogwarts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    private CardView housesCard, studentsCard, spellsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Defining cards
+        housesCard = findViewById(R.id.houses_card);
+        studentsCard = findViewById(R.id.students_card);
+        spellsCard = findViewById(R.id.spells_card);
+
+        //Add onClick listener to the cards
+        housesCard.setOnClickListener(this);
+        studentsCard.setOnClickListener(this);
+        spellsCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        //Switching screens on click event
+        switch (v.getId()){
+            case R.id.houses_card : i = new Intent(this, Houses.class); startActivity(i); break;
+            default: break;
+        }
     }
 }
---
+++ app/src/main/java/com/maringa/hogwarts/Houses.java
@@ -0,0 +1,14 @@
+package com.maringa.hogwarts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Houses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_houses);
+    }
+}
